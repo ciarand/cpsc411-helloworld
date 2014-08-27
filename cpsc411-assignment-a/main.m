519818cadfb89dafc19d5ac67a227ef3318c1aaa
Ciaran Downey <me@ciarand.me>
2014-08-27 09:58:20 -0700

    Initial Commit

---
+++ cpsc411-assignment-a/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cpsc411-assignment-a
+//
+//  Created by Ciaran Downey on 8/27/14.
+//  Copyright (c) 2014 Ciaran Downey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
